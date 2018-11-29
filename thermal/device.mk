5b969a0a6f1df3a145c146e0a2bf408d577e1b31
Ethan Chen <intervigil@gmail.com>
2018-11-28 20:41:20 -0800

    Disable userdebug diagnostics
    * We can't make use of most of the debug features on userdebug builds.
    
    Change-Id: I31df2090b31243b742c4e82dc08cd8491dada54f

--- thermal/device.mk
+++ thermal/device.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.pixel \
     thermal_symlinks
 
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
     thermal_logd
 endif
