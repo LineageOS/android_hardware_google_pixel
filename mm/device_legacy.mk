ce442c72ce07ab20e23e2fe72e91bf31ec2127ac
Ethan Chen <intervigil@gmail.com>
2018-11-28 20:41:20 -0800

    pixel: Disable userdebug diagnostics
    * We can't make use of most of the debug features on userdebug builds.
    
    Change-Id: I31df2090b31243b742c4e82dc08cd8491dada54f

--- mm/device_legacy.mk
+++ mm/device_legacy.mk
@@ -1,7 +1,7 @@
 PRODUCT_COPY_FILES += \
       hardware/google/pixel/mm/pixel-mm-legacy.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/pixel-mm-legacy.rc
 
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
     mm_logd
 endif
