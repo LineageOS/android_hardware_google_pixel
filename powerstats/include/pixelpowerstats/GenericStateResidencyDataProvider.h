bda0bf7862103472f8a1f7382a48705bfe01866e
Sujee Rajayogam <sujee@google.com>
2020-04-15 13:58:51 -0700

    Add features to PowerStatsHAL to support AoC stats
    PowerStatsHAL was extended to support the following:
    1. Merge states for the power entity from different sysfs nodes
    
    Bug: b/140217482
    Test: Manual on slider
    Change-Id: I66f8906cf9cf80ab04f9657606639475d33863ec

--- powerstats/include/pixelpowerstats/GenericStateResidencyDataProvider.h
+++ powerstats/include/pixelpowerstats/GenericStateResidencyDataProvider.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef HARDWARE_GOOGLE_PIXEL_POWERSTATS_GENERICSTATERESIDENCYDATAPROVIDER_H
 #define HARDWARE_GOOGLE_PIXEL_POWERSTATS_GENERICSTATERESIDENCYDATAPROVIDER_H
 
@@ -32,17 +48,19 @@ class PowerEntityConfig {
     PowerEntityConfig(const std::vector<StateResidencyConfig> &stateResidencyConfigs);
     PowerEntityConfig(const std::string &header,
                       const std::vector<StateResidencyConfig> &stateResidencyConfigs);
+    PowerEntityConfig(const uint32_t start_id, const std::string &header,
+                      const std::vector<StateResidencyConfig> &stateResidencyConfigs);
     std::string mHeader;
     std::vector<std::pair<uint32_t, StateResidencyConfig>> mStateResidencyConfigs;
 };
 
 class GenericStateResidencyDataProvider : public IStateResidencyDataProvider {
   public:
-    GenericStateResidencyDataProvider(std::string path) : mPath(std::move(path)){};
+    GenericStateResidencyDataProvider(std::string path) : mPath(std::move(path)) {}
     ~GenericStateResidencyDataProvider() = default;
     void addEntity(uint32_t id, const PowerEntityConfig &config);
-    bool getResults(std::unordered_map<uint32_t, PowerEntityStateResidencyResult>
-            &results) override;
+    bool getResults(
+            std::unordered_map<uint32_t, PowerEntityStateResidencyResult> &results) override;
     std::vector<PowerEntityStateSpace> getStateSpaces() override;
 
   private:
@@ -51,8 +69,8 @@ class GenericStateResidencyDataProvider : public IStateResidencyDataProvider {
 };
 
 std::vector<StateResidencyConfig> generateGenericStateResidencyConfigs(
-    const StateResidencyConfig &stateConfig,
-    const std::vector<std::pair<std::string, std::string>> &stateHeaders);
+        const StateResidencyConfig &stateConfig,
+        const std::vector<std::pair<std::string, std::string>> &stateHeaders);
 
 }  // namespace powerstats
 }  // namespace pixel
