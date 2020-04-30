d66253421b9a9d6848e9bc4d5213ba97fcc4f457
Harpreet \"Eli\" Sangha <eliptus@google.com>
2020-04-30 14:36:02 +0900

    vibrator: drv2624: consolidate sepolicy
    Test: Manual, Unit Test, VTS, PTS
    Signed-off-by: Harpreet \"Eli\" Sangha <eliptus@google.com>
    Change-Id: Ice723d6b263a01d2a5e0684bfa810109384ccc6f

--- vibrator/drv2624/device.mk
+++ vibrator/drv2624/device.mk
@@ -2,4 +2,5 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.drv2624 \
 
 BOARD_SEPOLICY_DIRS += \
+    hardware/google/pixel-sepolicy/vibrator/common \
     hardware/google/pixel-sepolicy/vibrator/drv2624 \
