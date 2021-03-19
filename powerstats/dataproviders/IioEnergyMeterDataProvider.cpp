004df5c1f00b1a36293ed715623e0775b3ca7c65
Benjamin Schwartz <bsschwar@google.com>
2021-03-19 11:38:15 -0700

    powerstats: Add init_rc and vintf_fragments to pixel impl
    Also removed log tags as they will be set by the service or parent library
    
    Bug: 183052760
    Test: dumpsys android.hardware.power.stats.IPowerStats/default
    Change-Id: I64f945e5c51a9f3c51c149e22a133d1d95ff1d79

--- powerstats/dataproviders/IioEnergyMeterDataProvider.cpp
+++ powerstats/dataproviders/IioEnergyMeterDataProvider.cpp
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power.stats-service.pixel"
-
 #include <dataproviders/IioEnergyMeterDataProvider.h>
 #include <dataproviders/IioEnergyMeterDataSelector.h>
 
