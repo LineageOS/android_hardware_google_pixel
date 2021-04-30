LineageOS/android_hardware_google_pixel

commit 9cf018372ba8527af4bc54d453f1e2a5d0a9749f

Usb Gadget HAL: modify monitorfs behavior
Currently, monitorffs will pull up gadget when the ffs
endpoints are ready. When the device change the data
role from peripheral to host, the pullup behavior will
still occur. It will cause the host mode and peripheral
mode start in the same time.

This patch will check USB state before pulling up
gadget.

Bug: 173086594
Test: build pass
Signed-off-by: Ray Chi <raychi@google.com>
Change-Id: Ic2a318f37d3c18e8f473ab85a996fea7f65194f1

usb/MonitorFfs.cpp
@@ -26,7 +26,7 @@ namespace usb {
 
 static volatile bool gadgetPullup;
 
-MonitorFfs::MonitorFfs(const char *const gadget)
+MonitorFfs::MonitorFfs(const char *const gadget, const char *const extconUsbState)
     : mWatchFd(),
       mEndpointList(),
       mLock(),
@@ -37,6 +37,7 @@ MonitorFfs::MonitorFfs(const char *const gadget)
       mCallback(NULL),
       mPayload(NULL),
       mGadgetName(gadget),
+      mExtconUsbState(extconUsbState),
       mMonitorRunning(false) {
     unique_fd eventFd(eventfd(0, 0));
     if (eventFd == -1) {
@@ -118,6 +119,7 @@ void *MonitorFfs::startMonitorFd(void *param) {
     bool writeUdc = true, stopMonitor = false;
     struct epoll_event events[kEpollEvents];
     steady_clock::time_point disconnect;
+    std::string usb_state;
 
     bool descriptorWritten = true;
     for (int i = 0; i < static_cast<int>(monitorFfs->mEndpointList.size()); i++) {
@@ -127,8 +129,17 @@ void *MonitorFfs::startMonitorFd(void *param) {
         }
     }
 
+    if (ReadFileToString(monitorFfs->mExtconUsbState, &usb_state))
+        usb_state = Trim(usb_state);
+    else
+        usb_state = TYPEC_GADGET_MODE_ENABLE;
+
     // notify here if the endpoints are already present.
-    if (descriptorWritten) {
+    if (usb_state == TYPEC_GADGET_MODE_DISABLE) {
+        gadgetPullup = false;
+        writeUdc = false;
+        ALOGI("pending GADGET pulled up due to USB disconnected");
+    } else if (descriptorWritten) {
         usleep(kPullUpDelay);
         if (!!WriteStringToFile(monitorFfs->mGadgetName, PULLUP_PATH)) {
             lock_guard<mutex> lock(monitorFfs->mLock);
@@ -172,7 +183,17 @@ void *MonitorFfs::startMonitorFd(void *param) {
                         }
                     }
 
-                    if (!descriptorPresent && !writeUdc) {
+                    if (ReadFileToString(monitorFfs->mExtconUsbState, &usb_state))
+                        usb_state = Trim(usb_state);
+                    else
+                        usb_state = TYPEC_GADGET_MODE_ENABLE;
+
+                    if (usb_state == TYPEC_GADGET_MODE_DISABLE) {
+                        if (kDebug)
+                            ALOGI("pending GADGET pulled up due to USB disconnected");
+                        gadgetPullup = false;
+                        writeUdc = false;
+                    } else if (!descriptorPresent && !writeUdc) {
                         if (kDebug)
                             ALOGI("endpoints not up");
                         writeUdc = true;

usb/include/pixelusb/UsbGadgetCommon.h
@@ -19,6 +19,7 @@
 
 #include <android-base/file.h>
 #include <android-base/properties.h>
+#include <android-base/strings.h>
 #include <android-base/unique_fd.h>
 
 #include <android/hardware/usb/gadget/1.0/IUsbGadget.h>
@@ -74,9 +75,13 @@ constexpr char kVendorRndisConfig[] = "vendor.usb.rndis.config";
 #define FUNCTION_NAME "function"
 #define FUNCTION_PATH CONFIG_PATH FUNCTION_NAME
 #define RNDIS_PATH FUNCTIONS_PATH "gsi.rndis"
+#define TYPEC_GADGET_MODE_DISABLE "0"
+#define TYPEC_GADGET_MODE_ENABLE "1"
 
 using ::android::base::GetProperty;
+using ::android::base::ReadFileToString;
 using ::android::base::SetProperty;
+using ::android::base::Trim;
 using ::android::base::unique_fd;
 using ::android::base::WriteStringToFile;
 using ::android::hardware::usb::gadget::V1_0::GadgetFunction;
@@ -126,11 +131,13 @@ class MonitorFfs {
   void *mPayload;
   // Name of the USB gadget. Used for pullup.
   const char *const mGadgetName;
+  // Extcon USB state from Type-C notification.
+  const char *const mExtconUsbState;
   // Monitor State
   bool mMonitorRunning;
 
  public:
-  MonitorFfs(const char *const gadget);
+  MonitorFfs(const char *const gadget, const char *const extconUsbState);
   // Inits all the UniqueFds.
   void reset();
   // Starts monitoring endpoints and pullup the gadget when

commit 8a02cb1253d13a6aff6e32f0c719c1341e0d525c

Usb Gadget HAL: Partial revert due to power regression
9cf018372ba8527af4bc54d453f1e2a5d0a9749f introduced a
power regression where adbd would keep spinning the CPU
waiting for FUNCTIONFS_BIND.

adbd    : timed out while waiting for FUNCTIONFS_BIND, trying again

Partially reverting the CL. Complete revert would have
to revert CLs across multiple devices.

Bug: 186824331
Test: adb while have usb booted. adb on unplug. adbd does not spin.
Change-Id: Ie666eb5318914a0b5b4e7edc14e0cda50f896f96

usb/MonitorFfs.cpp
@@ -26,7 +26,8 @@ namespace usb {
 
 static volatile bool gadgetPullup;
 
-MonitorFfs::MonitorFfs(const char *const gadget, const char *const extconUsbState)
+MonitorFfs::MonitorFfs(const char *const gadget,
+                       __attribute__((unused)) const char *const extconUsbState)
     : mWatchFd(),
       mEndpointList(),
       mLock(),
@@ -37,7 +38,6 @@ MonitorFfs::MonitorFfs(const char *const gadget, const char *const extconUsbStat
       mCallback(NULL),
       mPayload(NULL),
       mGadgetName(gadget),
-      mExtconUsbState(extconUsbState),
       mMonitorRunning(false) {
     unique_fd eventFd(eventfd(0, 0));
     if (eventFd == -1) {
@@ -119,7 +119,6 @@ void *MonitorFfs::startMonitorFd(void *param) {
     bool writeUdc = true, stopMonitor = false;
     struct epoll_event events[kEpollEvents];
     steady_clock::time_point disconnect;
-    std::string usb_state;
 
     bool descriptorWritten = true;
     for (int i = 0; i < static_cast<int>(monitorFfs->mEndpointList.size()); i++) {
@@ -129,17 +128,7 @@ void *MonitorFfs::startMonitorFd(void *param) {
         }
     }
 
-    if (ReadFileToString(monitorFfs->mExtconUsbState, &usb_state))
-        usb_state = Trim(usb_state);
-    else
-        usb_state = TYPEC_GADGET_MODE_ENABLE;
-
-    // notify here if the endpoints are already present.
-    if (usb_state == TYPEC_GADGET_MODE_DISABLE) {
-        gadgetPullup = false;
-        writeUdc = false;
-        ALOGI("pending GADGET pulled up due to USB disconnected");
-    } else if (descriptorWritten) {
+    if (descriptorWritten) {
         usleep(kPullUpDelay);
         if (!!WriteStringToFile(monitorFfs->mGadgetName, PULLUP_PATH)) {
             lock_guard<mutex> lock(monitorFfs->mLock);
@@ -183,17 +172,7 @@ void *MonitorFfs::startMonitorFd(void *param) {
                         }
                     }
 
-                    if (ReadFileToString(monitorFfs->mExtconUsbState, &usb_state))
-                        usb_state = Trim(usb_state);
-                    else
-                        usb_state = TYPEC_GADGET_MODE_ENABLE;
-
-                    if (usb_state == TYPEC_GADGET_MODE_DISABLE) {
-                        if (kDebug)
-                            ALOGI("pending GADGET pulled up due to USB disconnected");
-                        gadgetPullup = false;
-                        writeUdc = false;
-                    } else if (!descriptorPresent && !writeUdc) {
+                    if (!descriptorPresent && !writeUdc) {
                         if (kDebug)
                             ALOGI("endpoints not up");
                         writeUdc = true;

usb/include/pixelusb/UsbGadgetCommon.h
@@ -131,8 +131,6 @@ class MonitorFfs {
   void *mPayload;
   // Name of the USB gadget. Used for pullup.
   const char *const mGadgetName;
-  // Extcon USB state from Type-C notification.
-  const char *const mExtconUsbState;
   // Monitor State
   bool mMonitorRunning;
 