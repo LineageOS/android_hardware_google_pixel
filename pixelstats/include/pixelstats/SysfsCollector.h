73506acd51ad1d28a088fbdfd93a4a4c4f3059c4
George Lee <geolee@google.com>
2021-06-09 10:14:26 -0700

    pixelstats: enable mitigation stats
    Eldar ID: 279027215
    
    Bug: 186806028
    Test: Local Test.
    $>cmd stats print-logs
    $>logcat | grep <atom id>
    
    Signed-off-by: George Lee <geolee@google.com>
    Change-Id: I7e28b5eb1c51379b9a6bb64a0d52e7111351f7f4

--- pixelstats/include/pixelstats/SysfsCollector.h
+++ pixelstats/include/pixelstats/SysfsCollector.h
@@ -20,6 +20,7 @@
 #include <aidl/android/frameworks/stats/IStats.h>
 #include <hardware/google/pixel/pixelstats/pixelatoms.pb.h>
 #include "BatteryEEPROMReporter.h"
+#include "MitigationStatsReporter.h"
 #include "MmMetricsReporter.h"
 
 namespace android {
@@ -53,6 +54,7 @@ class SysfsCollector {
         const char *const ZramMmStatPath;
         const char *const ZramBdStatPath;
         const char *const EEPROMPath;
+        const char *const MitigationPath;
     };
 
     SysfsCollector(const struct SysfsPaths &paths);
@@ -105,9 +107,11 @@ class SysfsCollector {
     const char *const kZramMmStatPath;
     const char *const kZramBdStatPath;
     const char *const kEEPROMPath;
+    const char *const kPowerMitigationStatsPath;
 
     BatteryEEPROMReporter battery_EEPROM_reporter_;
     MmMetricsReporter mm_metrics_reporter_;
+    MitigationStatsReporter mitigation_stats_reporter_;
 
     // Proto messages are 1-indexed and VendorAtom field numbers start at 2, so
     // store everything in the values array at the index of the field number
