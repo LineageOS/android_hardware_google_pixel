1c2665ee4aa543ccb132a124372b298f7b784d33
Adam Shih <adamshih@google.com>
2021-07-13 14:13:56 +0800

    use aosp vibrator
    Bug: 193485183
    Test: boot
    Change-Id: Icae1e0072ccdfe1012702d76e4540e3d4fb899b2
    Merged-In: If71e1f7ed99256afb0828f3c9c1a570f5399d598

--- vibrator/drv2624/device.mk
+++ vibrator/drv2624/device.mk
@@ -1,5 +1,5 @@
 PRODUCT_PACKAGES += \
-    com.android.vibrator.drv2624 \
+    android.hardware.vibrator-service.drv2624
 
 BOARD_SEPOLICY_DIRS += \
     hardware/google/pixel-sepolicy/vibrator/common \
