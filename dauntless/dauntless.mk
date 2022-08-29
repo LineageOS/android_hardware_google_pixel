11e37bfdab4ff67f1bbf7b849c8b383549c97f95
Adam Shih <adamshih@google.com>
2022-08-29 13:51:00 +0800

    Simplify config by removing PRODUCT_COPY_FILES
    Bug: 242479757
    Test: build with /vendor/etc/permissions/android.hardware.strongbox_keystore.xml installed
    Change-Id: I1bce465751f936c6cb562e513d6746e7faee8826

--- dauntless/dauntless.mk
+++ dauntless/dauntless.mk
@@ -5,22 +5,12 @@ PRODUCT_PACKAGES += \
     citadel_updater \
     android.hardware.weaver@1.0-service.citadel \
     android.hardware.identity@1.0-service.citadel \
-    init_citadel
-
-# AIDL
-PRODUCT_PACKAGES += \
+    init_citadel \
+    android.hardware.strongbox_keystore.xml \
     android.hardware.security.keymint-service.citadel
 
-PRODUCT_COPY_FILES += \
-    hardware/google/pixel/dauntless/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.strongbox_keystore.xml
-
-ifneq ($(wildcard vendor/google_nos/provision),)
-PRODUCT_PACKAGES_DEBUG += CitadelProvision
-endif
-
 # USERDEBUG ONLY: Install test packages
-ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES_DEBUG += citadel_integration_tests \
                           pwntest \
-                          nugget_targeted_tests
-endif
+                          nugget_targeted_tests \
+			  CitadelProvision
