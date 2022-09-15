1498110a17151bef310b7d3c9c6936057dfc306f
ChengYou Ho <chengyouho@google.com>
2022-09-15 16:10:03 +0000

    Citadel.mk: install oemlock AIDL service
    Bug: 240932137
    Test: VtsHalOemLockTargetTest
    Change-Id: Ic2bd0f454125c3dd512b463255efaf7c65c3f24a

--- citadel/citadel.mk
+++ citadel/citadel.mk
@@ -6,6 +6,7 @@ PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0-service.citadel \
     android.hardware.authsecret-service.citadel \
     android.hardware.oemlock@1.0-service.citadel \
+    android.hardware.oemlock-service.citadel \
     android.hardware.weaver@1.0-service.citadel \
     android.hardware.keymaster@4.1-service.citadel \
     android.hardware.identity@1.0-service.citadel \
