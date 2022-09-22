5d0adab3d3be00e3c3595c81e1abd72225d90ae9
Jasna Marin <jasnamarin@google.com>
2022-09-22 15:17:03 +0000

    Added node count limit with tests.
    Bug: b/246317222.
    Test: atest libadaptivecpu_test:TreeReaderTest
    Change-Id: I534682808f3685dea9a614c2f5ddb8ef11202428

--- power-libperfmgr/adaptivecpu/tests/TreeReaderTest.cpp
+++ power-libperfmgr/adaptivecpu/tests/TreeReaderTest.cpp
@@ -158,6 +158,92 @@ TEST(TreeReaderTest, TreeReader_DeserializeProtoTree_CompareDifferent) {
     ASSERT_FALSE(*model == *deserializedTree);
 }
 
+void GenerateCompleteTree(int depth, proto::ModelTree *protoTree) {
+    proto::Node *node = nullptr;
+    if (depth == 0) {
+        node = protoTree->add_nodes();
+        proto::LeafNode *leafNode = node->mutable_leaf_node();
+        leafNode->set_decision(proto::ThrottleDecision::THROTTLE_70);
+    } else {
+        node = protoTree->add_nodes();
+        proto::SplitNode *splitNode = node->mutable_split_node();
+        splitNode->set_feature(proto::Feature::CPU_CORE_IDLE_TIME_PERCENT_1);
+        splitNode->set_value_index(2);
+        splitNode->set_threshold(12.345);
+
+        GenerateCompleteTree(depth - 1, protoTree);
+        GenerateCompleteTree(depth - 1, protoTree);
+    }
+}
+
+TEST(TreeReaderTest, TreeReader_DeserializeProtoTree_TooManyNodes) {
+    // Construct a proto tree with over MAX_NUM_NODES nodes.
+    proto::ModelTree protoTree;
+    // A complete tree of depth h-1 has 2^h - 1 nodes,
+    // so we build a tree deeper than log2(MAX_NUM_NODES + 1).
+    int h = std::log2(MAX_NUM_NODES * 4.0) + 1;
+    GenerateCompleteTree(h, &protoTree);
+
+    // Add feature statistics - 15 means and std devs.
+    for (int i = 0; i < 15; i++) {
+        proto::FeatureStatistic *stat = protoTree.add_feature_means();
+        stat->set_feature(proto::Feature(i));
+        stat->set_statistic(10.0);
+    }
+    for (int i = 0; i < 15; i++) {
+        proto::FeatureStatistic *stat = protoTree.add_feature_stds();
+        stat->set_feature(proto::Feature(i));
+        stat->set_statistic(1.0);
+    }
+
+    // Call function for deserializing proto tree.
+    std::unique_ptr<ModelTree> deserializedTree;
+    bool result = TreeReader::DeserializeProtoTreeToMemory(protoTree, &deserializedTree);
+    ASSERT_FALSE(result);
+}
+
+TEST(TreeReaderTest, TreeReader_DeserializeProtoTree_TreeTooDeep) {
+    // Construct a proto tree that is deeper than MAX_TREE_DEPTH
+    proto::ModelTree protoTree;
+    proto::Node *node = nullptr;
+    proto::SplitNode *splitNode = nullptr;
+    // Create an arbitrary amount (larger that MAX_TREE_DEPTH) of split nodes.
+    // Starting from the root, every new node is the left child of the previous node.
+    for (int i = 0; i < MAX_TREE_DEPTH + 100; i++) {
+        node = protoTree.add_nodes();
+        splitNode = node->mutable_split_node();
+        splitNode->set_feature(proto::Feature::CPU_CORE_IDLE_TIME_PERCENT_1);
+        splitNode->set_value_index(2);
+        splitNode->set_threshold(12.345);
+    }
+    // For the tree to be valid, create one leaf node more than the number of split nodes.
+    // The first leaf in sequence is the left child of the last split node, and the rest of
+    // them are right children, going from the last split node on the left, up to the root.
+    proto::LeafNode *leafNode = nullptr;
+    for (int i = 0; i < MAX_TREE_DEPTH + 101; i++) {
+        node = protoTree.add_nodes();
+        leafNode = node->mutable_leaf_node();
+        leafNode->set_decision(proto::ThrottleDecision::THROTTLE_70);
+    }
+
+    // Add feature statistics - 15 means and std devs.
+    for (int i = 0; i < 15; i++) {
+        proto::FeatureStatistic *stat = protoTree.add_feature_means();
+        stat->set_feature(proto::Feature(i));
+        stat->set_statistic(10.0);
+    }
+    for (int i = 0; i < 15; i++) {
+        proto::FeatureStatistic *stat = protoTree.add_feature_stds();
+        stat->set_feature(proto::Feature(i));
+        stat->set_statistic(1.0);
+    }
+
+    // Call function for deserializing proto tree.
+    std::unique_ptr<ModelTree> deserializedTree;
+    bool result = TreeReader::DeserializeProtoTreeToMemory(protoTree, &deserializedTree);
+    ASSERT_FALSE(result);
+}
+
 }  // namespace pixel
 }  // namespace impl
 }  // namespace power
