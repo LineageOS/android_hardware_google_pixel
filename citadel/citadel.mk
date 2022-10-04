c5029f9ea12b829149e04f93a47259e1284b3dee
Michael Bestas <mkbestas@lineageos.org>
2022-10-05 00:32:04 +0300

    citadel: Remove closed source targets
    Change-Id: I2a2247164855f6d70a9cf63f349331fc3558785b

--- citadel/citadel.mk
+++ citadel/citadel.mk
@@ -1,22 +1,13 @@
 # Citadel
 PRODUCT_PACKAGES += \
-    citadeld \
-    citadel_updater \
-    android.hardware.authsecret@1.0-service.citadel \
-    android.hardware.oemlock@1.0-service.citadel \
     android.hardware.weaver@1.0-service.citadel \
     android.hardware.keymaster@4.1-service.citadel \
-    android.hardware.identity@1.0-service.citadel \
     android.hardware.fastboot@1.1-impl.pixel \
     wait_for_strongbox
 
 # Citadel debug stuff
 PRODUCT_PACKAGES_DEBUG += \
     test_citadel
-
-# Resume on Reboot support
-PRODUCT_PACKAGES += \
-    android.hardware.rebootescrow-service.citadel
 
 # init scripts (won't be in AOSP)
 -include vendor/google_nos/init/citadel/init.mk
