c0a76a86a6fd64f304de96084b7bc068193a8097
Avichal Rakesh <arakesh@google.com>
2023-01-06 02:55:22 -0800

    Create common utils for AIDL Gadget HAL
    Initial GadgetHAL migration to AIDL missed migrating UsbGadgetUtils to
    AIDL. Although a UsbGadgetAidlUtils.h was added, it still relied on
    HIDL interface for the header's implementation.
    
    This CL separates out the logic common in HIDL and AIDL to CommonUtils
    and moves interface specific logic to hidl/ and aidl/ directories. It
    also adds libpixelusb-aidl build target for AIDL HALs to depend on.
    
    Note: UsbOverheatEvent still uses HIDL interfaces, so added a TODO for
    that.
    
    Bug: 218791946
    Test: Check logs to verify that AIDL HALs are using the correct
          implementation
    Change-Id: Ie63b62b5e612473993763dfd1640b404bc57414c

--- usb/aidl/UsbGadgetAidlUtils.cpp
+++ usb/aidl/UsbGadgetAidlUtils.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,14 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "libpixelusb"
+#define LOG_TAG "libpixelusb-aidl"
 
-#include "include/pixelusb/UsbGadgetCommon.h"
+#include <aidl/android/hardware/usb/gadget/GadgetFunction.h>
+#include <android-base/file.h>
+#include <android-base/properties.h>
+#include <utils/Log.h>
+
+#include "aidl/include/pixelusb/UsbGadgetAidlCommon.h"
 
 namespace android {
 namespace hardware {
@@ -24,116 +29,17 @@ namespace google {
 namespace pixel {
 namespace usb {
 
-int unlinkFunctions(const char *path) {
-    DIR *config = opendir(path);
-    struct dirent *function;
-    char filepath[kMaxFilePathLength];
-    int ret = 0;
-
-    if (config == NULL)
-        return -1;
-
-    // d_type does not seems to be supported in /config
-    // so filtering by name.
-    while (((function = readdir(config)) != NULL)) {
-        if ((strstr(function->d_name, FUNCTION_NAME) == NULL))
-            continue;
-        // build the path for each file in the folder.
-        snprintf(filepath, kMaxFilePathLength, "%s/%s", path, function->d_name);
-        ret = remove(filepath);
-        if (ret) {
-            ALOGE("Unable  remove file %s errno:%d", filepath, errno);
-            break;
-        }
-    }
-
-    closedir(config);
-    return ret;
-}
-
-int addEpollFd(const unique_fd &epfd, const unique_fd &fd) {
-    struct epoll_event event;
-    int ret;
-
-    event.data.fd = fd;
-    event.events = EPOLLIN;
-
-    ret = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event);
-    if (ret)
-        ALOGE("epoll_ctl error %d", errno);
-
-    return ret;
-}
-
-int linkFunction(const char *function, int index) {
-    char functionPath[kMaxFilePathLength];
-    char link[kMaxFilePathLength];
-
-    snprintf(functionPath, kMaxFilePathLength, "%s%s", FUNCTIONS_PATH, function);
-    snprintf(link, kMaxFilePathLength, "%s%d", FUNCTION_PATH, index);
-    if (symlink(functionPath, link)) {
-        ALOGE("Cannot create symlink %s -> %s errno:%d", link, functionPath, errno);
-        return -1;
-    }
-    return 0;
-}
+using ::aidl::android::hardware::usb::gadget::GadgetFunction;
+using ::android::base::GetProperty;
+using ::android::base::SetProperty;
+using ::android::base::WriteStringToFile;
 
 Status setVidPid(const char *vid, const char *pid) {
-    if (!WriteStringToFile(vid, VENDOR_ID_PATH))
-        return Status::ERROR;
-
-    if (!WriteStringToFile(pid, PRODUCT_ID_PATH))
-        return Status::ERROR;
-
-    return Status::SUCCESS;
-}
-
-std::string getVendorFunctions() {
-    if (GetProperty(kBuildType, "") == "user")
-        return "user";
-
-    std::string bootMode = GetProperty(PERSISTENT_BOOT_MODE, "");
-    std::string persistVendorFunctions = GetProperty(kPersistentVendorConfig, "");
-    std::string vendorFunctions = GetProperty(kVendorConfig, "");
-    std::string ret = "";
-
-    if (vendorFunctions != "") {
-        ret = vendorFunctions;
-    } else if (bootMode == "usbradio" || bootMode == "factory" || bootMode == "ffbm-00" ||
-               bootMode == "ffbm-01" || bootMode == "usbuwb") {
-        if (persistVendorFunctions != "")
-            ret = persistVendorFunctions;
-        else
-            ret = "diag";
-        // vendor.usb.config will reflect the current configured functions
-        SetProperty(kVendorConfig, ret);
-    }
-
-    return ret;
+    return setVidPidCommon(vid, pid) ? Status::SUCCESS : Status::ERROR;
 }
 
 Status resetGadget() {
-    ALOGI("setCurrentUsbFunctions None");
-
-    if (!WriteStringToFile("none", PULLUP_PATH))
-        ALOGI("Gadget cannot be pulled down");
-
-    if (!WriteStringToFile("0", DEVICE_CLASS_PATH))
-        return Status::ERROR;
-
-    if (!WriteStringToFile("0", DEVICE_SUB_CLASS_PATH))
-        return Status::ERROR;
-
-    if (!WriteStringToFile("0", DEVICE_PROTOCOL_PATH))
-        return Status::ERROR;
-
-    if (!WriteStringToFile("0", DESC_USE_PATH))
-        return Status::ERROR;
-
-    if (unlinkFunctions(CONFIG_PATH))
-        return Status::ERROR;
-
-    return Status::SUCCESS;
+    return resetGadgetCommon() ? Status::SUCCESS : Status::ERROR;
 }
 
 Status addGenericAndroidFunctions(MonitorFfs *monitorFfs, uint64_t functions, bool *ffsEnabled,
@@ -216,6 +122,7 @@ Status addAdb(MonitorFfs *monitorFfs, int *functionCount) {
 
     if (linkFunction("ffs.adb", (*functionCount)++))
         return Status::ERROR;
+
     monitorFfs->addEndPoint("/dev/usb-ffs/adb/ep1");
     monitorFfs->addEndPoint("/dev/usb-ffs/adb/ep2");
     ALOGI("Service started");
---
+++ usb/include/pixelusb/CommonUtils.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HARDWARE_GOOGLE_PIXEL_USB_UTILSCOMMON_H_
+#define HARDWARE_GOOGLE_PIXEL_USB_UTILSCOMMON_H_
+
+#include <android-base/unique_fd.h>
+
+#include <string>
+
+namespace android {
+namespace hardware {
+namespace google {
+namespace pixel {
+namespace usb {
+
+constexpr int kBufferSize = 512;
+constexpr int kMaxFilePathLength = 256;
+constexpr int kEpollEvents = 10;
+constexpr bool kDebug = false;
+constexpr int kDisconnectWaitUs = 100000;
+constexpr int kPullUpDelay = 500000;
+constexpr int kShutdownMonitor = 100;
+
+constexpr char kBuildType[] = "ro.build.type";
+constexpr char kPersistentVendorConfig[] = "persist.vendor.usb.usbradio.config";
+constexpr char kVendorConfig[] = "vendor.usb.config";
+constexpr char kVendorRndisConfig[] = "vendor.usb.rndis.config";
+
+#define GADGET_PATH "/config/usb_gadget/g1/"
+#define PULLUP_PATH GADGET_PATH "UDC"
+#define PERSISTENT_BOOT_MODE "ro.bootmode"
+#define VENDOR_ID_PATH GADGET_PATH "idVendor"
+#define PRODUCT_ID_PATH GADGET_PATH "idProduct"
+#define DEVICE_CLASS_PATH GADGET_PATH "bDeviceClass"
+#define DEVICE_SUB_CLASS_PATH GADGET_PATH "bDeviceSubClass"
+#define DEVICE_PROTOCOL_PATH GADGET_PATH "bDeviceProtocol"
+#define DESC_USE_PATH GADGET_PATH "os_desc/use"
+#define OS_DESC_PATH GADGET_PATH "os_desc/b.1"
+#define CONFIG_PATH GADGET_PATH "configs/b.1/"
+#define FUNCTIONS_PATH GADGET_PATH "functions/"
+#define FUNCTION_NAME "function"
+#define FUNCTION_PATH CONFIG_PATH FUNCTION_NAME
+#define RNDIS_PATH FUNCTIONS_PATH "gsi.rndis"
+
+// Adds the given fd to the epollfd(epfd).
+int addEpollFd(const ::android::base::unique_fd &epfd, const ::android::base::unique_fd &fd);
+// Extracts vendor functions from the vendor init properties.
+std::string getVendorFunctions();
+// Removes all the usb functions link in the specified path.
+int unlinkFunctions(const char *path);
+// Creates a configfs link for the function.
+int linkFunction(const char *function, int index);
+// Sets the USB VID and PID. Returns true on success, false on failure
+bool setVidPidCommon(const char *vid, const char *pid);
+// Pulls down USB gadget. Returns true on success, false on failure
+bool resetGadgetCommon();
+}  // namespace usb
+}  // namespace pixel
+}  // namespace google
+}  // namespace hardware
+}  // namespace android
+
+#endif  // HARDWARE_GOOGLE_PIXEL_USB_UTILSCOMMON_H_
