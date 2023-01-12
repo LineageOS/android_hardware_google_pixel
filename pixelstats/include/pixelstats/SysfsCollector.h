976abc9b934017be829ef99447197681a10ee19d
Doug Zobel <zobel@google.com>
2023-01-11 18:28:18 -0600

    Add PcieLinkStatsReported for PCIe link statistics logging
    eldar/887079995
    
    Test: adb logcat "pixelstats-vendor:D *:S"
    Bug: 264287533
    Change-Id: Icaa15b264fbf2f0a368411c5125fd9984cc76f66
    Merged-In: Icaa15b264fbf2f0a368411c5125fd9984cc76f66
    Signed-off-by: Doug Zobel <zobel@google.com>

--- pixelstats/include/pixelstats/SysfsCollector.h
+++ pixelstats/include/pixelstats/SysfsCollector.h
@@ -69,6 +69,8 @@ class SysfsCollector {
         const char *const TempResidencyPath;
         const char *const LongIRQMetricsPath;
         const char *const ResumeLatencyMetricsPath;
+        const char *const ModemPcieLinkStatsPath;
+        const char *const WifiPcieLinkStatsPath;
     };
 
     SysfsCollector(const struct SysfsPaths &paths);
@@ -111,6 +113,7 @@ class SysfsCollector {
     void logVendorAudioHardwareStats(const std::shared_ptr<IStats> &stats_client);
     void logVendorLongIRQStatsReported(const std::shared_ptr<IStats> &stats_client);
     void logVendorResumeLatencyStats(const std::shared_ptr<IStats> &stats_client);
+    void logPcieLinkStats(const std::shared_ptr<IStats> &stats_client);
 
     const char *const kSlowioReadCntPath;
     const char *const kSlowioWriteCntPath;
@@ -142,6 +145,8 @@ class SysfsCollector {
     const char *const kTempResidencyPath;
     const char *const kLongIRQMetricsPath;
     const char *const kResumeLatencyMetricsPath;
+    const char *const kModemPcieLinkStatsPath;
+    const char *const kWifiPcieLinkStatsPath;
 
     BatteryEEPROMReporter battery_EEPROM_reporter_;
     MmMetricsReporter mm_metrics_reporter_;
