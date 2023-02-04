3a014fe51a1a168665704ff5abd92d5efdd5ad3a
Tai Kuo <taikuo@google.com>
2023-02-04 15:05:52 +0800

    cs40l26: correct mk statement
    Bug: 264625320
    Test: Build pass on related projects.
    Change-Id: Ib8e6b5825f294072d6eabc53a2b73e8ac05c34e0

--- vibrator/cs40l26/device-stereo.mk
+++ vibrator/cs40l26/device-stereo.mk
@@ -1,6 +1,7 @@
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.cs40l26 \
-    android.hardware.vibrator-service.cs40l26-dual \
+    android.hardware.vibrator-service.cs40l26-dual
 
 BOARD_SEPOLICY_DIRS += \
-    hardware/google/pixel-sepolicy/vibrator/cs40l26 \
+    hardware/google/pixel-sepolicy/vibrator/common \
+    hardware/google/pixel-sepolicy/vibrator/cs40l26
