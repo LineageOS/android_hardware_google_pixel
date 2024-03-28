0548004bd6aa91cdf4ab894f1c1ba727823d2025
Jenny Ho <hsiufangho@google.com>
2024-03-28 14:57:32 +0800

    pixelstats: add additional data for relaxation reporting
    Bug: 331503894
    Change-Id: I2a24676962b6a71e4147e5606503570fe132f704
    Merged-In: I2a24676962b6a71e4147e5606503570fe132f704
    Signed-off-by: Jenny Ho <hsiufangho@google.com>
    (cherry picked from commit a44bbed918926bfaf0b986a933912441d96b9836)

--- pixelstats/include/pixelstats/BatteryEEPROMReporter.h
+++ pixelstats/include/pixelstats/BatteryEEPROMReporter.h
@@ -129,6 +129,7 @@ class BatteryEEPROMReporter {
     const int kNumBatteryHistoryFields = 19;
     /* The number of elements for relaxation event */
     const int kNumFGLearningFields = 10;
+    const int kNumFGLearningFieldsV2 = 15;
     /* The number of elements for history validation event */
     const int kNumHistoryValidationFields = 4;
 
