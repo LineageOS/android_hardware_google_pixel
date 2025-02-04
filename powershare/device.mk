15b40a4ff00b81ec952d9b14ba4a1d69c0a90c91
Michael Bestas <mkbestas@lineageos.org>
2025-02-04 17:34:58 +0200

    pixel: Switch to common lineage powershare HAL
    Change-Id: Iea6b78ac68511152954f4ada5ca618801dbd7947

--- powershare/device.mk
+++ powershare/device.mk
@@ -1,5 +1,7 @@
 # PowerShare
+TARGET_POWERSHARE_PATH := /sys/class/power_supply/wireless/device/rtx
+
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.pixel
+    vendor.lineage.powershare@1.0-service.default
 
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powershare
