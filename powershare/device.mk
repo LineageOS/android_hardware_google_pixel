11245652cfb1a9e3f52f71ec621e47bfa8654c1c
Bruno Martins <bgcngm@gmail.com>
2025-04-24 21:36:24 +0100

    pixel: Switch to PowerShare AIDL
    Change-Id: I97400638493a3b9d5a3b87fa6831c9affc7c3def

--- powershare/device.mk
+++ powershare/device.mk
@@ -2,6 +2,6 @@
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/power_supply/wireless/device/rtx)
 
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.default
+    vendor.lineage.powershare-service.default
 
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powershare
