9b0e0f571a697d576c7c21f9ed1b8387a0a05575
Bruno Martins <bgcngm@gmail.com>
2025-04-25 15:22:14 +0100

    touch: Migrate to AIDL
    Change-Id: I7fc7992744df049835213c58261eb81579d6f3c1

--- touch/GloveMode.cpp
+++ touch/GloveMode.cpp
@@ -1,34 +1,40 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
-
-#include <android-base/properties.h>
 
 #include "GloveMode.h"
 
-#define TOUCH_SENSITIVITY_PROP "persist.vendor.touch_sensitivity_mode"
+#include <android-base/properties.h>
 
-namespace vendor::lineage::touch::pixel {
+using ::android::base::GetBoolProperty;
+using ::android::base::SetProperty;
 
-// Methods from ::vendor::lineage::touch::V1_0::IGloveMode follow.
-Return<bool> GloveMode::isEnabled() {
-    return android::base::GetBoolProperty(TOUCH_SENSITIVITY_PROP, false);
+namespace {
+
+constexpr const char* kTouchSensitivityProp = "persist.vendor.touch_sensitivity_mode";
+
+}  // anonymous namespace
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace touch {
+
+ndk::ScopedAStatus GloveMode::getEnabled(bool* _aidl_return) {
+    bool enabled = GetBoolProperty(kTouchSensitivityProp, false);
+
+    *_aidl_return = enabled;
+    return ndk::ScopedAStatus::ok();
 }
 
-Return<bool> GloveMode::setEnabled(bool enabled) {
-    return android::base::SetProperty(TOUCH_SENSITIVITY_PROP, enabled ? "1" : "0");
+ndk::ScopedAStatus GloveMode::setEnabled(bool enable) {
+    SetProperty(kTouchSensitivityProp, enable ? "1" : "0");
+
+    return ndk::ScopedAStatus::ok();
 }
 
-}  // namespace vendor::lineage::touch::pixel
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
